28766d1f14b20be4ac7ffedfffcab519cb597483
G_ji <guruttl90@gmail.com>
2023-03-14 16:20:15 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import React, {Component} from "react";
+import { render } from "react-dom";
+
+class Welcome extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <h1>Meet the <i style={{ color: "SteelBlue" }}>{this.props.name}</i></h1>;
+  }
+}
+/*const Welcome = (props)=>{
+  return <h1>Meet the <i style={{ color: "SteelBlue" }}>{props.name}</i></h1>;
+};*/
+
 function App() {
-  const name = 'StarGazers'
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name = "StarGazers"/>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
